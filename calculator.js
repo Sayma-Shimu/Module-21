51976d26360475a1acea100182cdf696a57296a6
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-08-11 18:55:23 +0600

    calculator

---
+++ calculator.js
@@ -0,0 +1,45 @@
+function add(num1, num2){
+    return num1 +num2;
+}
+
+function substract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+} 
+
+function divide(num1, num2){
+    const vag = num1 / num2;
+    return vag;
+}
+
+
+function calculator(a,b, operation){
+    if(operation === 'add'){
+        const result = add(a, b);
+        return result;
+    }
+    else if(operation === 'substract'){
+        const result = substract(a,b)
+        return result;
+    }
+    else if(operation === 'multiply'){
+        const result = multiply(a,b);
+        return result;
+    }
+    else if(operation === 'divide'){
+        const result = divide(a,b)
+        return result;
+    }
+    else{
+        return "only 'add', 'subatract', 'multiply' and 'divided' function allowed "
+    }
+
+}
+
+const result = calculator(10, 15, 'add')
+console.log(result);
+
+
